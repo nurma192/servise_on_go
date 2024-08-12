5cad4381a6a84a0a1d3ffee6e9b16fe1f5eaa627
nurma192 <nurma192k@gmail.com>
2024-08-12 21:22:35 +0500

    created logger with log/slog

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"service_on_go/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -11,11 +19,37 @@ func main() {
 	fmt.Println(cfg)
 
 	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
 
-	// TODO: init storage: sqlLite
+	//log = log.With("Project realized from nurma192", 192) // mozhno dobavit eshe parametry is sebiya
+	log.Info("Starter url is shorter!!!", slog.String("env", cfg.Env))
+	log.Debug("Debug messages unenabled")
+
+	// TODO: init storage: postgresql, gorm
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 
 }
